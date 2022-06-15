a7dcd08b8de6ad619cc8864eb25216028c7c1f67
NoahAncolie <noahancolie@gmail.com>
2022-06-15 19:39:52 +0200

    début index

--- src/components/CofeeCrud/IndexCofees/index.jsx
+++ src/components/CofeeCrud/IndexCofees/index.jsx
@@ -1,27 +1,43 @@
 import React from "react";
 import { useState } from "react";
+import { useEffect } from "react";
 
 const IndexCofees = () => {
 
-    const [cofeeList, setList] = useState()
+    const [cofeeList, setList] = useState("")
+    const [showList, setShow] = useState("")
 
     const prepList = (list) => {
-        setList(list)
+        setList(JSON.stringify(list))
+        setShow(cofeeList)
     }
 
-    const list = () => {
+    const filter = (value) => {
+        setShow(JSON.stringify(JSON.parse(cofeeList).filter(elem => (elem.name.includes(value)))))
+    }
+
+    useEffect(() => {
         fetch("https://mycoffees.herokuapp.com/products", {
             method: "get",
             headers: {
                 "Content-Type": "application/json",
             }
         }).then((response) => { return (response.json()) })
-            .then((response) => prepList(JSON.stringify(response)))
-    }
+            .then((response) => {prepList(response)
+            console.log(response)
+            console.log(cofeeList)})
+    }, []);
 
     return (
         <>
-            <button onClick={list}>CLICK ME</button>
+            <form class="search-form" id="search-form">
+                <input type="text" id="search-bar" name="search-bar" onChange={event => {filter(event.target.value)}}></input>
+            </form>
+            <div>
+                {showList.length > 1 ? JSON.parse(cofeeList).map(elem => (
+                    <p>{elem.name}</p>
+                )) : <></>}
+            </div>
         </>
     )
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Logout } from "../../apiCalls/LogoutCall";
 
 const Header = () => {
     return (
         <header className="App-header">
             <h1>Hello, voici un Header</h1>
+            <button onClick={Logout}>Disconnect</button>
         </header>
     )
 }
